9ae6cdf75fe893620d75d9526eec85f9686274fa
Dev-Ndovu <nurusoundchapel@gmail.com>
2026-06-21 14:29:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/futuresound/player/MainActivity.kt
+++ app/src/main/java/com/futuresound/player/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
